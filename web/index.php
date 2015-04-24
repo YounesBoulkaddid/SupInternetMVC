761f56d66b58112a8b0bfc0a7434bc5980932f3e
Younesta <boulkaddid.younes@gmail.com>
2015-04-24 17:03:52 +0200

    creation du routing

--- web/index.php
+++ web/index.php
@@ -11,26 +11,33 @@
  *
  *
  */
-$routes = ...
+require_once __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\Yaml\Parser;
+$yaml = new Parser();
+$routes = $yaml->parse(file_get_contents('../app/config/routing.yml'));
+if(isset($_GET['p'])) {
+    $currentRoute = $routes[$_GET['p']]['controller'];
+    $routesArray = explode(':', $currentRoute);
+    var_dump($routesArray);
 //ControllerClassName, end name is ...Controller
-$controller_class = ... ;
+    $controller_class = $routesArray[0];
 
 //ActionName, end name is ...Action
-$action_name = ...;
+    $action_name = $routesArray[1];
 
-$controller = new $controller_class();
+    $controller = new $controller_class();
 
 //$Request can by an object
-$request['request'] = &$_POST;
-$request['query'] = &$_GET;
+    $request['request'] = &$_POST;
+    $request['query'] = &$_GET;
 //...
 
 //$response can be an object
-$response = $controller->$action_name($request);
+    $response = $controller->$action_name($request);
 
-
-/**
- * Use Twig !
- */
-require $response['view'];
+    /**
+     * Use Twig !
+     */
+    require $response['view'];
+}
